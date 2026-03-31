0e16f473a639d0aea55600d854dab5ac085682a1
daejung2606 <daejung2606@dge.go.kr>
2026-03-31 10:32:11 +0900

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,5 +1,12 @@
 package jumptojava;
 
 public class Test {
+	public static void main(String[] args) {
+		System.out.println("Hello java");
+		System.out.println(a.contais("java"));
+		System.out.println(a.charAt(5));
+		System.out.println(a.replaceAll("java","world"));
+		System.out.println(a.substrig(3, 8));
+	}
 
 }
