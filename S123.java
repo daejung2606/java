e634eea1f8b2de41d6724813dcca85409719347c
daejung2606 <daejung2606@dge.go.kr>
2026-04-03 12:26:09 +0900

    Add files via upload

--- S123.java
+++ S123.java
@@ -2,8 +2,13 @@ package jumptojava;
 
 public class S123 {
 	public static void main(String[] args) {
-		String a = "a:b:c:d";
-		String[] result = a.split(":");
-
+		String[] week = new String[7];
+		weeks[0] = "월";
+		weeks[1] = "화";
+		weeks[2] = "수";
+		weeks[3] = "목";
+		weeks[4] = "금";
+		weeks[5] = "토";
+		weeks[6] = "일";
 	}
 }
